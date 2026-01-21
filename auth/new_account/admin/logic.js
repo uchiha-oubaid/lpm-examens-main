f406552b01133f043b447c3d54c99ff7f1e2bb56
= <=>
2026-01-21 17:38:01 +0100

    adding the javaScript needs

---
+++ auth/new_account/admin/logic.js
@@ -0,0 +1,7 @@
+const connectBtn = document.getElementById("button");
+const usernameInput = document.getElementById("username");
+const emailInput = document.getElementById("email");
+const numberInput = document.getElementById("number");
+const passwordInput = document.getElementById("password");
+const passwordOInput = document.getElementById("passwordO");
+const codeInput = document.getElementById("code");
---
+++ backend/dbConnect.js
@@ -0,0 +1,14 @@
+const {createPool} = require("mysql");
+
+const pool = createPool({
+    host: "localhost",
+    user: "oubaid",
+    password: "YourPassword123!",
+    connectionLimit: 10
+});
+
+function selectColumns() {
+    pool.query(`SELECT * FROM lpmDataBase.users`, (err, res) => {
+        console.log(res);
+    });
+}
